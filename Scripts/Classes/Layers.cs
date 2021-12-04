542dd44619fee1f90e2d0e3c4ac18bc43730b49c
Alex Kovács <kovacsalex95@gmail.com>
2021-12-04 14:33:09 +0100

    Fixed transform reference link issue

--- Scripts/Classes/Layers.cs
+++ Scripts/Classes/Layers.cs
@@ -157,9 +157,9 @@ namespace lxkvcs
             organicity = target.organicity;
             minDistance = target.minDistance;
 
-            _position = target._position;
-            _rotation = target._rotation;
-            _scale = target._scale;
+            _position = new RandomVector3(target._position);
+            _rotation = new RandomVector3(target._rotation);
+            _scale = new RandomVector3(target._scale);
 
             prefabs = new PlaceObject[target.prefabs.Length];
             for (int i = 0; i < prefabs.Length; i++)
